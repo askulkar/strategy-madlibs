54de6fe78610537ff1e54c2cd8f85bb3743a34f1
Aniket Kulkarni <anikekulkarni@paypal.com>
2020-08-28 21:57:01 -0700

    Update parts.rb

--- parts.rb
+++ parts.rb
@@ -2,11 +2,11 @@ module MadLibs
   PARTS_OF_SPEECH = {
     nouns:        %w(competitive\ advantage learning\ organization container\ platform public\ cloud Genesis Titanium GCP
                    big\ data growth value digital\ business payments\ platform geo\ strategy
-                   platform revolution culture ecosystem internet\ of\ things monitoring\ platform
-                   insight\ from\ data machine\ learning artificial\ intelligence blockchain design\ thinking digital\ transformation),
+                   platform revolution culture ecosystem monitoring\ platform
+                   insight\ from\ data machine\ learning artificial\ intelligence design\ thinking digital\ transformation),
     plural_nouns: %w(social\ media leaders data\ leaders ),
     adjectives:   %w(digital\ first networked collaborative cloud\ based secure highly\ converged global
-                   customer\ focused disruptive sustainable agile open secure reliable universe\ class mobile\ first
+                   customer\ focused disruptive sustainable agile open secure reliable resilient  fault\tolerant universe\ class mobile\ first
                    innovative)
   }
 end
