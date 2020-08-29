655156048af0f04581398f576694ff216cb0a846
Aniket Kulkarni <anikekulkarni@paypal.com>
2020-08-28 22:01:42 -0700

    Update parts.rb

--- parts.rb
+++ parts.rb
@@ -4,7 +4,7 @@ module MadLibs
                    big\ data growth value digital\ business payments\ platform geo\ strategy Venmo Hyperwallet Braintree Xoom
                    platform revolution culture ecosystem monitoring\ platform
                    insight\ from\ data machine\ learning artificial\ intelligence design\ thinking digital\ transformation),
-    plural_nouns: %w(social\ media leaders data\ leaders ),
+    plural_nouns: %w(social\ media leaders talented\ eSRE\ developers ),
     adjectives:   %w(digital\ first networked collaborative cloud\ based secure highly\ converged global
                    customer\ focused disruptive sustainable agile open secure reliable resilient  fault\tolerant universe\ class mobile\ first
                    innovative)
