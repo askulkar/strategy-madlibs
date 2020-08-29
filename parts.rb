2112acda1703d5f3996cc340c374e7603386a0a6
Aniket Kulkarni <anikekulkarni@paypal.com>
2020-08-28 21:47:11 -0700

    Update parts.rb

--- parts.rb
+++ parts.rb
@@ -1,7 +1,7 @@
 module MadLibs
   PARTS_OF_SPEECH = {
     nouns:        %w(competitive\ advantage learning\ organization container \platform public \cloud private \cloud
-                   big\ data growth value digital\ business
+                   big\ data growth value digital\ business payments \platform geo \strategy
                    platform revolution culture ecosystem internet\ of\ things 
                    insight\ from\ data virtual\ reality artificial\ intelligence blockchain design\ thinking digital\ transformation),
     plural_nouns: %w(social\ media leaders data\ leaders),
