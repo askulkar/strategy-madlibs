e6f2b59c74b273a898ebe54d23a3251b6e5c1242
Aniket Kulkarni <anikekulkarni@paypal.com>
2021-02-24 19:34:06 -0800

    Update parts.rb

--- parts.rb
+++ parts.rb
@@ -1,10 +1,10 @@
 module MadLibs
   PARTS_OF_SPEECH = {
     nouns:        %w(competitive\ advantage learning\ organization container\ platform public\ cloud\ migration Genesis Titanium control\ lifecycle\ management foundational\ platform
-                   big\ data growth value digital\ business payments\ platform geo\ strategy Xoom\ convergence MTTR
-                   platform revolution culture ecosystem monitoring\ platform blameless\ postmortems error\ budget Corp\ Infrastructure
+                   big\ data growth value digital\ business payments\ platform geo\ strategy Xoom\ convergence Braintree\ convergence MTTR
+                   platform revolution culture ecosystem monitoring\ platform blameless\ postmortems error\ budget SLOs Corp\ Infrastructure
                    insight\ from\ data machine\ learning artificial\ intelligence design\ thinking digital\ transformation),
-    plural_nouns: %w(SRE\ leaders talented\ eSRE\ developers Command\ Center ),
+    plural_nouns: %w(SRCE\ leaders talented\ eSRE\ developers Command\ Center ),
     adjectives:   %w(digital\ first networked collaborative cloud\ based secure highly\ converged global
                    customer\ focused disruptive sustainable agile open secure reliable resilient fault\ tolerant universe\ class mobile\ first
                    innovative)
